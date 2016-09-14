b6fe90e02ba114edde180fe68e25d42bb09927e4
Eduardo González <fonologico@gmail.com>
2016-09-14 21:00:50 +0200

    implementada NotesCollectionView

--- HackerBooksPro/NotesCollectionViewController.m
+++ HackerBooksPro/NotesCollectionViewController.m
@@ -7,16 +7,41 @@
 //
 
 #import "NotesCollectionViewController.h"
+#import "Note.h"
+#import "NoteViewCell.h"
 
 @interface NotesCollectionViewController ()
 
 @end
 
+static NSString *cellId = @"noteCell";
+
 @implementation NotesCollectionViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+
+    [self registerCell];
+
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+
+    Note *note = [self.fetchedResultsController objectAtIndexPath:indexPath];
+
+    NoteViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:cellId
+                                                                        forIndexPath:indexPath];
+    [cell observeNote:note];
+
+    return cell;
+}
+
+-(void)registerCell{
+
+    UINib *nib = [UINib nibWithNibName:@"NoteViewCell"
+                                bundle:nil];
+    [self.collectionView registerNib:nib
+          forCellWithReuseIdentifier:cellId];
 }
 
 @end
